ae4d089c500f7827e6d79a611ece2cec396c2254
salybu <rkdmsdl93@gmail.com>
2021-10-13 13:42:50 +0900

    feat: (server) Login 호출 route 추가

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // express 모듈 불러오기
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+const jwt = require("./jwt");
 
 // express 객체 생성
 const app = express();
@@ -13,23 +15,38 @@ app.listen(port);
 const fs = require("fs");
 let db = fs.readFileSync("db.json", "utf-8");
 
-// 미들웨어 함수를 특정 경로에 등록
-// app.use('/api/data', function(req, res) {
-//     res.json({ greeting: 'Hello World' });
-// });
-
 app.use(bodyParser.json());
-
-app.get("/users", function (req, res) {
-  const DB = JSON.parse(db).users;
-  res.json(DB);
-});
 
 app.post("/user/login", function (req, res) {
   const login = req.body;
   const DB = JSON.parse(db).users;
-  console.log(login, "  login");
-  res.json(login);
+
+  let loginSuccess = false;
+  DB.forEach((user) => {
+    if (login.userName == user.userName && login.password == user.password) {
+      loginSuccess = true;
+      return false;
+    }
+  });
+
+  if (loginSuccess) {
+    const accessToken = jwt.sign(login);
+
+    res.status(200).send({
+      tokenType: "Bearer",
+      expiresIn: 3600,
+      accessToken,
+    });
+  } else {
+    res.status(400).send({
+      message: "Invalid username/password supplied",
+    });
+  }
+});
+
+app.get("/users", function (req, res) {
+  const DB = JSON.parse(db).users;
+  res.json(DB);
 });
 
 console.log(`server running at http ${port}`);
