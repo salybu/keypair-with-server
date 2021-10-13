290cc7237f873460f91b567fe2b3b1630a2b9e03
salybu <rkdmsdl93@gmail.com>
2021-10-13 23:35:57 +0900

    feat: token reducer 추가

--- client/src/modules/index.ts
+++ client/src/modules/index.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
+import token, { tokenSaga } from './token';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  token,
+});
 
 export default rootReducer;
 
 export function* rootSaga() {
-  yield all([]);
+  yield all([tokenSaga()]);
 }
---
+++ client/src/modules/token.ts
@@ -0,0 +1,58 @@
+import { Action, createActions, handleActions } from 'redux-actions';
+import { call, put, takeEvery } from '@redux-saga/core/effects';
+import { ILogin, ILoginOutput, ITokenState, IUserToken, RootState } from 'types';
+import { userApi } from 'apis';
+
+const initialState: ITokenState = {
+  token: null,
+  userName: null,
+  loading: false,
+  error: null,
+};
+
+const prefix = 'keypair-with-server/token';
+
+export const { pending, success, fail } = createActions('PENDING', 'SUCCESS', 'FAIL', { prefix });
+
+const reducer = handleActions<ITokenState, IUserToken>(
+  {
+    PENDING: (state) => ({
+      ...state,
+      loading: true,
+      error: null,
+    }),
+    SUCCESS: (state, action) => {
+      const { token, userName } = action.payload;
+      return { ...state, loading: false, token, userName, error: null };
+    },
+    FAIL: (state, action: any) => ({
+      ...state,
+      loading: false,
+      error: action.payload,
+    }),
+  },
+  initialState,
+  { prefix },
+);
+
+export default reducer;
+
+export const selectToken = (state: RootState): ITokenState => state.token;
+
+// saga
+export const { login } = createActions('LOGIN', { prefix });
+
+function* loginSaga(action: Action<ILogin>) {
+  try {
+    yield put(pending());
+    const res: ILoginOutput = yield call(userApi.login, action.payload);
+    const params = { token: res.accessToken, userName: action.payload.userName };
+    yield put(success(params));
+  } catch (error: any) {
+    // yield put(fail(new Error(error.response.data.error || 'UNKNOWN_ERROR')));
+  }
+}
+
+export function* tokenSaga() {
+  yield takeEvery(`${prefix}/LOGIN`, loginSaga);
+}
