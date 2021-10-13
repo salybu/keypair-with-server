c9d04ec1f2f17a1699fec65bd2a85e5e13976d7c
salybu <rkdmsdl93@gmail.com>
2021-10-13 13:40:55 +0900

    feat: (client) Login API 및 proxy route 추가

--- client/src/apis/userApi.ts
+++ client/src/apis/userApi.ts
@@ -1,19 +1,15 @@
 import axios from 'axios';
-// const USER_API_URL = 'https://localhost:5000';
-// const USER_API_URL = 'https://keypair-mock-server.herokuapp.com/';
+import { ILogin } from 'types';
 
 export default class userApi {
-  //   public static async login(reqData: string): Promise<string> {
-  public static async login() {
-    const data = { userName: 'user1', password: 'aaabbb' };
-    // const response = await axios.post(USER_API_URL + `/users`, data);
-    // const response = await axios({ method: 'post', url: USER_API_URL + `/users`, headers: {}, data: { userName: 'user1', password: 'aaabbb' } });
-    // const response = await axios.get(USER_API_URL + `/users`);
-    // const response = await axios.get(`/users`);
-    const response = await axios.post(`/user/login`, data);
-    // const response = await axios({ method: 'post', url: `/user/login`, headers: {}, data: { userName: 'user1', password: 'aaabbb' } });
-    console.log(response.data);
-
-    // return response.data;
+  public static async login({ userName, password }: ILogin) {
+    try {
+      const response = await axios.post(`/user/login`, { userName, password }, { headers: { 'Content-Type': 'application/json', Accept: 'application/json' } });
+      if (response.status == 200) {
+        return response.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
